4899120f4d11620df44ae2472597e1b2a7514d01
Olasubomi Odekunle <subomi.odekunle@flutterwavego.com>
2024-03-13 09:20:00 +0100

    I added readme

--- src/main/java/com/example/fraudDetection/entity/TransactionEvent.java
+++ src/main/java/com/example/fraudDetection/entity/TransactionEvent.java
@@ -4,6 +4,8 @@ package com.example.fraudDetection.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 //import ja.persistence.*;
 
 @Entity
@@ -18,9 +20,24 @@ public class  TransactionEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message =  "timestamp cannot be blank")
+    @NonNull
+    @Column(nullable = false)
     private Long timestamp;
+
+    @NotBlank(message =  "amount cannot be blank")
+    @NonNull
+    @Column(nullable = false)
     private Double amount;
+
+    @NotBlank(message =  "userID cannot be blank")
+    @NonNull
+    @Column(nullable = false)
     private String userID;
+
+    @NotBlank(message =  "serviceId cannot be blank")
+    @NonNull
+    @Column(nullable = false)
     private String serviceID;
 
 }
